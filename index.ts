1ed605bcf9d09153ade5150448e3efe7fbf9dd6c
Andres6936 <andres6936@gmail.com>
2024-08-18 21:27:03 -0500

    Replace local file scanning with remote HTML fetching
    This update switches the logic from scanning local files to fetching HTML content from a remote URL. The new approach fetches pages based on a regex match from a specified HTML file and processes them similarly. Commented out the old file-scanning logic for potential future reference.

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import {Glob} from 'bun'
-
+// import {Glob} from 'bun'
+//
 type ResponseClearHtml = {
     errorMessages: string[],
     fileName: string,
@@ -19,23 +19,52 @@ async function getClearHtml(html: string): Promise<ResponseClearHtml> {
     return await stream.json();
 }
 
-(async () => {
-    const glob = new Glob('src/pages/**');
-    for (const file of glob.scanSync('.')) {
-        try {
-            const responseOf = await getClearHtml(await Bun.file(file).text());
-            if (responseOf.errorMessages.length > 0) {
-                console.error(responseOf.errorMessages);
-            } else {
-                const htmlOf = responseOf.result;
-                await Bun.write(file, htmlOf);
-                console.log(`${file} cleared`)
-            }
+//
+// (async () => {
+//     const glob = new Glob('src/pages/**');
+//     for (const file of glob.scanSync('.')) {
+//         try {
+//             const responseOf = await getClearHtml(await Bun.file(file).text());
+//             if (responseOf.errorMessages.length > 0) {
+//                 console.error(responseOf.errorMessages);
+//             } else {
+//                 const htmlOf = responseOf.result;
+//                 await Bun.write(file, htmlOf);
+//                 console.log(`${file} cleared`)
+//             }
+//
+//             // Wait for 3 seconds
+//             await new Promise(resolve => setTimeout(resolve, 3000));
+//         } catch (error) {
+//             console.error(error)
+//         }
+//     }
+// })()
 
-            // Wait for 3 seconds
-            await new Promise(resolve => setTimeout(resolve, 3000));
-        } catch (error) {
-            console.error(error)
+(async () => {
+    const regexOf = /href="([A-Z]\w+).html"/g;
+    const parts = (await Bun.file('src/pages/NotasBiblicas.astro').text()).matchAll(regexOf);
+    if (!parts) {
+        console.error('Not group capture for the text')
+        return;
+    }
+
+    for (const part of parts) {
+        const hrefOfPage = part[1];
+        const stream = await fetch(`https://www.bibleserralta.com/${hrefOfPage}.html`);
+        const buffer = await stream.arrayBuffer();
+        const decoder = new TextDecoder('iso-8859-1')
+
+        const responseOf = await getClearHtml(decoder.decode(buffer));
+        if (responseOf.errorMessages.length > 0) {
+            console.error(responseOf.errorMessages);
+        } else {
+            const htmlOf = responseOf.result;
+            await Bun.write(`src/pages/${hrefOfPage}.astro`, htmlOf);
+            console.log(`${hrefOfPage} cleared`)
         }
+
+        // Wait for 3 seconds
+        await new Promise(resolve => setTimeout(resolve, 3000));
     }
 })()
