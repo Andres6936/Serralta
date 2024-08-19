24a1b267eff84cc3af325df21f0bd90405004a91
Andres6936 <andres6936@gmail.com>
2024-08-18 21:55:17 -0500

    Comment out HTML processing and fetching logic
    Temporarily disabled the HTML clearing and fetching function by commenting out the relevant code in `index.ts`. This includes the function that processes HTML using an external API and the script logic that matches and processes HTML files from a specific URL.

--- index.ts
+++ index.ts
@@ -1,24 +1,24 @@
 // import {Glob} from 'bun'
 //
-type ResponseClearHtml = {
-    errorMessages: string[],
-    fileName: string,
-    result: string,
-}
-
-async function getClearHtml(html: string): Promise<ResponseClearHtml> {
-    const stream = await fetch('https://api.htmlwasher.com/public/html-processing/process-html-by-text', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
-            'Html': html,
-        })
-    });
-    return await stream.json();
-}
-
+// type ResponseClearHtml = {
+//     errorMessages: string[],
+//     fileName: string,
+//     result: string,
+// }
+//
+// async function getClearHtml(html: string): Promise<ResponseClearHtml> {
+//     const stream = await fetch('https://api.htmlwasher.com/public/html-processing/process-html-by-text', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded'
+//         },
+//         body: new URLSearchParams({
+//             'Html': html,
+//         })
+//     });
+//     return await stream.json();
+// }
+//
 //
 // (async () => {
 //     const glob = new Glob('src/pages/**');
@@ -40,31 +40,31 @@ async function getClearHtml(html: string): Promise<ResponseClearHtml> {
 //         }
 //     }
 // })()
-
-(async () => {
-    const regexOf = /href="([A-Z]\w+).html"/g;
-    const parts = (await Bun.file('src/pages/NotasBiblicas.astro').text()).matchAll(regexOf);
-    if (!parts) {
-        console.error('Not group capture for the text')
-        return;
-    }
-
-    for (const part of parts) {
-        const hrefOfPage = part[1];
-        const stream = await fetch(`https://www.bibleserralta.com/${hrefOfPage}.html`);
-        const buffer = await stream.arrayBuffer();
-        const decoder = new TextDecoder('iso-8859-1')
-
-        const responseOf = await getClearHtml(decoder.decode(buffer));
-        if (responseOf.errorMessages.length > 0) {
-            console.error(responseOf.errorMessages);
-        } else {
-            const htmlOf = responseOf.result;
-            await Bun.write(`src/pages/${hrefOfPage}.astro`, htmlOf);
-            console.log(`${hrefOfPage} cleared`)
-        }
-
-        // Wait for 3 seconds
-        await new Promise(resolve => setTimeout(resolve, 3000));
-    }
-})()
+//
+// (async () => {
+//     const regexOf = /href="([A-Z]\w+).html"/g;
+//     const parts = (await Bun.file('src/pages/NotasBiblicas.astro').text()).matchAll(regexOf);
+//     if (!parts) {
+//         console.error('Not group capture for the text')
+//         return;
+//     }
+//
+//     for (const part of parts) {
+//         const hrefOfPage = part[1];
+//         const stream = await fetch(`https://www.bibleserralta.com/${hrefOfPage}.html`);
+//         const buffer = await stream.arrayBuffer();
+//         const decoder = new TextDecoder('iso-8859-1')
+//
+//         const responseOf = await getClearHtml(decoder.decode(buffer));
+//         if (responseOf.errorMessages.length > 0) {
+//             console.error(responseOf.errorMessages);
+//         } else {
+//             const htmlOf = responseOf.result;
+//             await Bun.write(`src/pages/${hrefOfPage}.astro`, htmlOf);
+//             console.log(`${hrefOfPage} cleared`)
+//         }
+//
+//         // Wait for 3 seconds
+//         await new Promise(resolve => setTimeout(resolve, 3000));
+//     }
+// })()
