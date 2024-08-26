82735a44886979b92ec05b22ca24a04428d8449c
Andres6936 <andres6936@gmail.com>
2024-08-25 23:09:35 -0500

    Update absolute paths and modify config for environment
    Changed relative image paths to absolute paths in index.astro for better resource loading. Updated astro.config.mjs to dynamically set the base path based on the environment mode.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import {defineConfig} from 'astro/config'
 
 export default defineConfig({
     site: 'https://andres6936.github.io',
-    base: 'Serralta',
+    base: import.meta.env.MODE === 'production' ? 'Serralta' : '',
 })
