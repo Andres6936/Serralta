36dd791303c3d54c54c7fb6b0dd2739069d9aa98
Andres6936 <andres6936@gmail.com>
2024-08-31 09:33:22 -0500

    Fix URL path concatenation and base URL handling.
    Removed trailing slashes from URL paths to ensure proper concatenation and included `base` in href attributes for correct navigation. Adjusted the `astro.config.mjs` to handle base URL correctly depending on the environment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import {defineConfig} from 'astro/config'
 
 export default defineConfig({
     site: 'https://andres6936.github.io/Serralta',
-    base: import.meta.env.PROD ? '/Serralta' : '',
+    base: import.meta.env.PROD ? 'Serralta' : '',
     i18n: {
         locales: ['en', 'es'],
         defaultLocale: 'en',
