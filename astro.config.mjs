589865a47601065d83761fa1b4bffe88b91960d4
Andres6936 <andres6936@gmail.com>
2024-08-31 09:08:52 -0500

    Update site and base URL in astro.config.mjs
    Modify site URL to include 'Serralta' and adjust base URL logic for production. This ensures consistent routing and correct relative link handling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import {defineConfig} from 'astro/config'
 
 export default defineConfig({
-    site: 'https://andres6936.github.io',
-    base: import.meta.env.PROD ? 'Serralta' : '',
+    site: 'https://andres6936.github.io/Serralta',
+    base: import.meta.env.PROD ? '/Serralta' : '',
     i18n: {
         locales: ['en', 'es'],
         defaultLocale: 'en',
