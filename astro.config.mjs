f82e6279744108faef878af038a7550fdf325f7e
Andres6936 <andres6936@gmail.com>
2026-05-28 20:29:18 -0500

    Upgrade to Astro 6 and migrate to Master CSS v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import {defineConfig} from 'astro/config'
+import { defineConfig } from "astro/config";
+import masterCSS from "@master/css.astro";
 
 export default defineConfig({
-    site: 'https://andres6936.github.io/Serralta',
-    base: import.meta.env.PROD ? 'Serralta' : '',
-    i18n: {
-        locales: ['en', 'es'],
-        defaultLocale: 'en',
-    }
-})
+  site: "https://andres6936.github.io/Serralta",
+  base: import.meta.env.PROD ? "Serralta" : "",
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+  },
+  integrations: [masterCSS()],
+});
